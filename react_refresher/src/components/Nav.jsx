f6429d8fa31d58be9d3c88a90a2a6ce8263a7b86
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-10-27 21:34:35 -0400

    made a few components

---
+++ react_refresher/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
